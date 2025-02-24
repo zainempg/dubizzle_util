61fbf96c01eac869a432cdac774ff1803d035fce
zainempg <zainul.abideen@dubizzlelabs.com>
2025-02-25 01:09:28 +0500

    ``` feat: Update build configurations and remove unused index.html
    This commit updates the Gradle build configurations and removes the unused `index.html` file from
    ``````
     the utility library.
    
    -   **Removed `util/index.html`:**
        -   Deleted the `index.html` file from the `util` directory, indicating that this file was likely unused or no longer required for the documentation or project structure.
    -   **Updated Gradle:**
        -
    ``````
      updated `dokka` version to `2.0.0`
    -   **Updated string:**
        - updated app name to `Dubizzle Util`
        - added `org.jetbrains.dokka.experimental.tryK2=true`
    ```

--- util/build.gradle.kts
+++ util/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     kotlin("plugin.serialization") version "2.1.10"
     id("maven-publish")
-    id("org.jetbrains.dokka") version "1.8.10" // Add Dokka plugin
+    id("org.jetbrains.dokka") version "2.0.0" // Add Dokka plugin
 
 }
 
